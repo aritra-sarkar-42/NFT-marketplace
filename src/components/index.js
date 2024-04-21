a46a9d4e6d5c29c15984037af521a2b97064cf96
Aritra Sarkar <144362932+aritra-sarkar-42@users.noreply.github.com>
2024-04-21 15:30:09 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,15 @@
 import  EarthCanvas  from './canvas/Earth';
 import Navbar from "./Navbar";
 import Contact from "./Contact";
+import Contact2 from "./Contact2"
 import CanvasLoader from "./Loader";
-
+import Contact3 from "./ForgotPassword"
 
 export {
   Navbar,
   Contact,
+  Contact2,
+  Contact3,
   CanvasLoader,
   EarthCanvas, 
 };
